31abc301dfb15aa561415aa008b5d079d5bdadfd
Doctor-Z <diming.zhang@gmail.com>
2024-11-11 00:57:50 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -2,4 +2,4 @@
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 # DIY扩展 在此脚本 增加插件
 
-bash <(curl -Lso- https://raw.githubusercontent.com/gd0772/patch/main/x86.sh)
+bash <(curl -Lso- https://raw.githubusercontent.com/iProfessorZ/patch/refs/heads/main/x86.sh)
